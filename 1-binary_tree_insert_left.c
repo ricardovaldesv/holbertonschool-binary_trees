fb75a5914de59700a9264f96b66a7f4ebe300e9f
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-12 09:13:06 -0700

    function 1 insert left v10

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,10 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	 binary_tree_t *new_node;
-	 binary_tree_t *current_node;
-	
-	 if (parent == NULL)
+	binary_tree_t *new_node;
+	binary_tree_t *current_node;
+
+	if (parent == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
