6e3b74db076a522bb4791e3ee68b4a8867669a10
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-12 10:33:12 -0700

    function 3. Delete v2

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,14 +4,14 @@
  *binary_tree_delete- Function that deletes an entire binary tree
  *
  *@tree:  is a pointer to the root node of the tree to delete
- *Return: nothing 
+ *Return: nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	
+
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
