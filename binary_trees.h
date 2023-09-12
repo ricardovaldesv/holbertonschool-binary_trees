2816c78d36ab0d4d5de45d721aaa8ae8a7323f2a
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-12 13:34:37 -0700

    function 6. Pre-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -33,6 +33,8 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
+void print_num(int n);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -52,5 +54,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 
 #endif
