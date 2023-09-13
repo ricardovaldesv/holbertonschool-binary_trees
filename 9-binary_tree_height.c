faf4f4a2c7e5585aab4c7f5712874028068a783f
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-13 06:44:45 -0700

    function 9. Height v2

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t cont_L= 0, cont_R = 0;
+	size_t cont_L = 0, cont_R = 0;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
@@ -18,11 +18,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	cont_R += binary_tree_height(tree->right);
 
 	if (tree->left == NULL)
-		return(1);
+		return (1);
 	else if (tree->right == NULL)
 		return (1);
 	else if (cont_L > cont_R)
-		return(cont_L + 1);
+		return (cont_L + 1);
 	else
-		return(cont_R + 1);
+		return (cont_R + 1);
 }
