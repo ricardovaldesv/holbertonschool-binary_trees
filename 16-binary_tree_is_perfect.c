3abe342710d104fe4dd9328442f4ef5026055b49
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-14 07:35:56 -0700

    function 16 betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -90,9 +90,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 
 /**
- *binary_tree_is_full- Function  checks if a binary tree is full
+ *binary_tree_is_perfect- Function that checks if a binary tree is perfect.
  *
- *@tree:  is a pointer to the root node of the tree to delete.
+ *@tree:  is a pointer  to the root node of the tree to check.
  *Return: If tree is NULL, return 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
