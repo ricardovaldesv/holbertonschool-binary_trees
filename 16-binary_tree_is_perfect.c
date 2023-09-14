a7b610c4702408d1f306da83cc317a320f502338
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-13 18:00:12 -0700

    function 16. Is perfect v2

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,13 +1,6 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_is_full- Function  checks if a binary tree is full
- *
- *@tree:  is a pointer to the root node of the tree to delete.
- *Return: If tree is NULL, return 0
- */
-
-/**
  * binary_tree_height - goes through a binary tree using pre-order trave
  * @tree: pointer to the root node of the tree to traverse
  * Return: measures the height of a binary tree
@@ -54,6 +47,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
@@ -62,6 +58,36 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+ *binary_tree_size- Function that that measures the size of a binary tree.
+ *
+ *@tree:  is a pointer to the root node of the tree to measure the size.
+ *Return: If tree is NULL, your function must return 0.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t cont_L = 0, cont_R = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	cont_L += binary_tree_size(tree->left);
+	cont_R += binary_tree_size(tree->right);
+
+	if (tree->left == NULL)
+		return (2);
+	else if (tree->right == NULL)
+		return (2);
+	else
+		return (cont_L + cont_R + 1);
+}
+
+
 
 /**
  *binary_tree_is_full- Function  checks if a binary tree is full
@@ -74,7 +100,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
+	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1
+			&& (binary_tree_size(tree->left) - binary_tree_size(tree->right) == 0))
 		return (1);
 	else
 		return (0);
